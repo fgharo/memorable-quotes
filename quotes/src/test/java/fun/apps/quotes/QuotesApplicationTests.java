9d61c8836c0a18184f1fa6157e203c5ea4866f2f
fgharo <fharo@redhat.com>
2020-06-15 15:42:49 -0700

    Adding a spring boot rest service project and an angularjs ui project to represent application implementation.

---
+++ quotes/src/test/java/fun/apps/quotes/QuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.apps.quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
